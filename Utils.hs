e1098067a88bce236aa93e45f0b7844641c7b074
Laurent Peuch <cortex@worlddomination.be>
2013-08-30 23:30:20 +0200

    [enh] new helper: foldlEMap

--- Utils.hs
+++ Utils.hs
@@ -1,6 +1,7 @@
 module Utils
 ( elemToStr
 , foldlESeq
+, foldlEMap
 , nodeStr
 ) where
 
@@ -9,6 +10,11 @@ import Data.Yaml.Syck
 foldlESeq :: (a -> YamlElem -> a) -> a -> YamlElem -> a
 foldlESeq function acc (ESeq []) = acc
 foldlESeq function acc (ESeq (x:xs)) = foldlESeq function (function acc $ n_elem x) (ESeq xs)
+
+foldlEMap :: (a -> (YamlElem, YamlElem) -> a) -> a -> YamlElem -> a
+foldlEMap function acc (EMap []) = acc
+foldlEMap function acc (EMap ((key, value):xs)) = foldlEMap function (function acc content) (EMap xs)
+    where content = (n_elem key, n_elem value)
 
 elemToStr :: YamlElem -> String
 elemToStr (EStr string) = unpackBuf string
--- td.hs
+++ td.hs
@@ -6,8 +6,7 @@ data Todo = Todo { desciption :: String
                  } deriving (Show)
 
 getDictKeys :: YamlElem -> [String]
-getDictKeys (EMap []) = []
-getDictKeys (EMap ((key, _):xs)) = (elemToStr $ n_elem key):(getDictKeys (EMap xs))
+getDictKeys = foldlEMap (\acc (key, value) -> elemToStr key:acc) []
 
 printKeys :: YamlNode -> IO ()
 printKeys = putStr . unlines . getDictKeys . n_elem
