da617d99e4a6b68a162d4692084666d1ef085277
Laurent Peuch <cortex@worlddomination.be>
2013-09-01 18:11:17 +0200

    [enh] playing arround

--- td.hs
+++ td.hs
@@ -11,16 +11,25 @@ getDictKeys = foldlEMap (\acc (key, value) -> elemToStr key:acc) []
 printKeys :: YamlNode -> IO ()
 printKeys = putStr . unlines . getDictKeys . n_elem
 
-parseTodos :: YamlElem -> [String]
-parseTodos = foldlESeq (\acc elem -> acc ++ [getDescription elem]) []
+parseTodos :: YamlElem -> [Todo]
+parseTodos = foldlESeq (\acc elem -> acc ++ [elemToTodo elem]) []
+
+elemToTodo :: YamlElem -> Todo
+elemToTodo elem = Todo {description = (getDescription elem), done = False}
+
+todosToString :: [Todo] -> [String]
+todosToString todoList = map (\todo -> "[" ++ (if done todo then "X" else " ") ++ "] " ++ description todo) todoList
 
 getDescription :: YamlElem -> String
-getDescription (EMap []) = ""
-getDescription (EMap ((key, value):xs))
-    | nodeStr key == "description" = nodeStr value
-    | otherwise               = getDescription (EMap xs)
+getDescription todo = elemToStr $ getValue  todo testKey (EStr $ packBuf "")
 
-printDescriptions = putStr . unlines . parseTodos . n_elem
+testKey :: YamlElem -> Bool
+testKey key = (elemToStr key) == "description"
+
+numberise :: [String] -> [String]
+numberise s = map (\(number, string) -> (show number) ++ " - " ++ string) (zip [1..] s)
+
+printDescriptions = putStr . unlines . numberise . todosToString . parseTodos . n_elem
 
 main = do
     content <- parseYamlFile "pouet.yaml"
